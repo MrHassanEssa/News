5406a69432d2145d88c454044accb744bc85ac59
hassan <hassan.y.essa@gmail.com>
2018-11-14 13:01:35 +0200

    Added Project

---
+++ app/src/main/java/com/example/m_t_m/news/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.m_t_m.news;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    String API_KEY = "8190df9eb51445228e397e4185311a66"; // ### YOUE NEWS API HERE ###
+   static String  NEWS_SOURCE
+           ;
+
+
+    ListView listNews;
+    ProgressBar loader;
+    ArrayList<HashMap<String, String>> dataList = new ArrayList<HashMap<String, String>>();
+    static final String KEY_AUTHOR = "author";
+    static final String KEY_TITLE = "title";
+    static final String KEY_DESCRIPTION = "description";
+    static final String KEY_URL = "url";
+    static final String KEY_URLTOIMAGE = "urlToImage";
+    static final String KEY_PUBLISHEDAT = "publishedAt";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listNews =  findViewById(R.id.listNews);
+        loader =findViewById(R.id.loader);
+        listNews.setEmptyView(loader);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        getSource(sharedPreferences);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+
+
+
+        if(Function.isNetworkAvailable(getApplicationContext()))
+        {
+            DownloadNews newsTask = new DownloadNews();
+            newsTask.execute();
+        }else{
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+
+    }
+     void getSource(SharedPreferences sharedPreferences){
+        NEWS_SOURCE=sharedPreferences.getString(getString(R.string.pref_source_key),getString(R.string.pref_bbc_news_value));
+    }
+    static String returnSource(){
+        return NEWS_SOURCE;
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       MenuInflater inflater=getMenuInflater();
+       inflater.inflate(R.menu.visualizer_menu,menu);
+       return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int idOfMenu=item.getItemId();
+        if(idOfMenu == R.id.action_settings){
+            Intent intent =new Intent(this,Setting.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals(getString(R.string.pref_source_key))) {
+            //   NEWS_SOURCE=getResources().getString(R.string.pref_bbc_news_title);
+            getSource(sharedPreferences);
+        }
+        Intent intent = getIntent();
+        startActivity(intent);    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister VisualizerActivity as an OnPreferenceChangedListener to avoid any memory leaks.
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+     class DownloadNews extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+        protected String doInBackground(String... args) {
+            String xml = "";
+
+            String urlParameters = "";
+            xml = Function.excuteGet("https://newsapi.org/v1/articles?source="+returnSource()+"&sortBy=top&apiKey="+API_KEY, urlParameters);
+            return  xml;
+        }
+        @Override
+        protected void onPostExecute(String xml) {
+
+            if(xml.length()>10){ // Just checking if not empty
+
+                try {
+                    JSONObject jsonResponse = new JSONObject(xml);
+                    JSONArray jsonArray = jsonResponse.optJSONArray("articles");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put(KEY_AUTHOR, jsonObject.optString(KEY_AUTHOR).toString());
+                        map.put(KEY_TITLE, jsonObject.optString(KEY_TITLE).toString());
+                        map.put(KEY_DESCRIPTION, jsonObject.optString(KEY_DESCRIPTION).toString());
+                        map.put(KEY_URL, jsonObject.optString(KEY_URL).toString());
+                        map.put(KEY_URLTOIMAGE, jsonObject.optString(KEY_URLTOIMAGE).toString());
+                        map.put(KEY_PUBLISHEDAT, jsonObject.optString(KEY_PUBLISHEDAT).toString());
+                        dataList.add(map);
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Unexpected error", Toast.LENGTH_SHORT).show();
+                }
+
+                ListNewsAdapter adapter = new ListNewsAdapter(MainActivity.this, dataList);
+                listNews.setAdapter(adapter);
+
+                listNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        Intent i = new Intent(MainActivity.this, DetailsActivity.class);
+                        i.putExtra("url", dataList.get(+position).get(KEY_URL));
+                        startActivity(i);
+                    }
+                });
+
+            }else{
+                Toast.makeText(getApplicationContext(), "No news found", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+    }
+
+
+
+}
